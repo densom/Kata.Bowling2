3298d853bfc76486f14366877fc3bbb40947fc53
Dennis Somerville <dasomerville@sherwin.com>
2013-10-04 18:52:11 -0400

    Work in progress.

--- Kata.Bolwing2.Tests/BowlingGameTests.cs
+++ Kata.Bolwing2.Tests/BowlingGameTests.cs
@@ -5,17 +5,60 @@ namespace Kata.Bowling2.Tests
     [TestFixture]
     public class BowlingGameTests
     {
-        [Test]
-        [TestCase(new[] {1,1}, 2)]
-        public void Score_OpenFrame_ScoresPinsKnockedDown(int[] rolls, int expectedScore)
+        private BowlingGame _game;
+        private RollHelper _rollHelper;
+
+        [SetUp]
+        public virtual void SetUp()
         {
-            var game = new BowlingGame();
-            var rollHelper = new RollHelper(game);
-            rollHelper.RollAll(rolls);
-            
-            Assert.That(game.Score(), Is.EqualTo(expectedScore));
+            _game = new BowlingGame();
+            _rollHelper = new RollHelper(_game);
         }
 
+        [Test]
+        [TestCase(new[] {1}, null)]
+        [TestCase(new[] {1,1}, 2)]
+        public void Score_OpenFrame_ScoresPinsKnockedDown(int[] rolls, int? expectedScore)
+        {
+            _rollHelper.RollAll(rolls);
+            Assert.That(_game.Score(), Is.EqualTo(expectedScore));
+        }
+
+        [Test]
+        public void Score_Spare_GetsOneBonusBall()
+        {
+            _rollHelper.RollAll(new[] {9,1,2,0});
+            Assert.That(_game.Score(), Is.EqualTo(14));
+        }
+
+        [Test]
+        [TestCase(new[] {1}, 1)]
+        [TestCase(new[] {1,1}, 0)]
+        [TestCase(new[] {1,1,1}, 1)]
+        public void CurrentFrameScore_KeepsTrackOfCurrentFrame(int[] rolls, int expectedCurrentFrameScore)
+        {
+            _rollHelper.RollAll(rolls);
+            Assert.That(_game.CurrentFrameScore, Is.EqualTo(expectedCurrentFrameScore));
+        }
+
+        [Test]
+        [TestCase(new[] {1}, false)]
+        [TestCase(new[] {1,1}, true)]
+        public void IsFirstBall_OpenFrame_ReturnsTrueOnFirstThrowInNewFrame(int[] rolls, bool isFirstBallExpected)
+        {
+            _rollHelper.RollAll(rolls);
+            Assert.That(_game.IsFirstBall(), Is.EqualTo(isFirstBallExpected));
+        }
+
+        [Test]
+        [TestCase(new[] {1}, null)]
+        [TestCase(new[] {1,1}, 2)]
+//        [TestCase(new[] {1,1}, 2)]
+        public void Score_CalculatesAtEndOfFrame_OpenFrame(int[] rolls, int? expectedScore)
+        {
+            _rollHelper.RollAll(rolls);
+            Assert.That(_game.Score(), Is.EqualTo(expectedScore));
+        }
 
     }
 }
--- Kata.Bowling2/BowlingGame.cs
+++ Kata.Bowling2/BowlingGame.cs
@@ -1,17 +1,77 @@
-﻿namespace Kata.Bowling2
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata.Bowling2
 {
     public class BowlingGame
     {
         private int _score;
+        private bool _isFirstBall;
+        private Queue<int> _bonusSpareBalls = new Queue<int>();
+
+        public BowlingGame()
+        {
+            _isFirstBall = true;
+        }
+
+        internal int CurrentFrameScore { get; private set; }
 
         public void Roll(int pinsKnockedDown)
         {
+            _isFirstBall = !_isFirstBall;
+
+            CalculateCurrentFrameScore(pinsKnockedDown);
+
+            ScorePendingBonusBalls();
+
+            if (IsSpare(pinsKnockedDown))
+            {
+                _bonusSpareBalls.Enqueue(pinsKnockedDown);
+            }
+
             _score += pinsKnockedDown;
+
         }
 
-        public int Score()
+        private void ScorePendingBonusBalls()
         {
+            if (_bonusSpareBalls.Count > 0)
+            {
+                _score += _bonusSpareBalls.Dequeue();
+            }
+            
+        }
+
+        private void CalculateCurrentFrameScore(int pinsKnockedDown)
+        {
+            if (_isFirstBall)
+            {
+                CurrentFrameScore = 0;
+            }
+            else
+            {
+                CurrentFrameScore += pinsKnockedDown;
+            }
+        }
+
+        private bool IsSpare(int pinsKnockedDown)
+        {
+            return (CurrentFrameScore + pinsKnockedDown) == 10;
+        }
+
+        public int? Score()
+        {
+            if (!_isFirstBall)
+            {
+                return null;
+            }
             return _score;
         }
+
+        internal bool IsFirstBall()
+        {
+            return _isFirstBall;
+        }
+
     }
 }
